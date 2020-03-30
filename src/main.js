7d427f005ef1db91cdfe2deb887255925e58973b
Jeremy Perry <jeremy.m.perry@gmail.com>
2020-03-30 08:47:03 -0400

    html

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ startTest = () => {
 getNewQuestion = () => {
   if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     //action after questions are completed
-    return window.location.assign("/end.html");
+    if(!alert('Thank you for tyring out my app! More features coming soon!')){window.location.reload();}
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
